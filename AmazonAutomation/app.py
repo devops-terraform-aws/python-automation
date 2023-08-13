c13e226a8287a4eeac409d5fe28c46b6093c985f
developer <ukohae39@gmail.com>
2023-08-13 10:18:27 -0400

    refactoring code

--- AmazonAutomation/app.py
+++ AmazonAutomation/app.py
@@ -3,8 +3,6 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 from time import sleep
 
@@ -19,51 +17,25 @@ product_price = []
 def get_webpage():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     driver.get("https://www.amazon.com/")
+    # keyword = input("Enter item name: ")
     keyword = "PS5"
-    
-    # Use WebDriverWait to wait for the search box to be present
-    wait = WebDriverWait(driver, 10)
-    search_box = wait.until(EC.presence_of_element_located((By.ID, 'twotabsearchtextbox')))
+    search_box = driver.find_element(By.ID, 'twotabsearchtextbox')
     search_box.send_keys(keyword)
 
     search_button = driver.find_element(By.ID, 'nav-search-submit-button')
     search_button.click()
-    
-    # Use WebDriverWait to wait for the price elements to be present
-    wait.until(EC.presence_of_element_located((By.XPATH, './/span[@class="a-price-whole"]')))
-    wait.until(EC.presence_of_element_located((By.XPATH, './/span[@class="a-price-fraction"]')))
-    
+    driver.implicitly_wait(5)
+    sleep(5)
+
     item_price = driver.find_elements(By.XPATH, './/span[@class="a-price-whole"]')
     item_fraction = driver.find_elements(By.XPATH, './/span[@class="a-price-fraction"]')
 
-    if item_price and item_fraction:
+    if item_price !=[] and item_fraction !=[]:
         final_price = '.'.join([item_price[0].text, item_fraction[0].text])
     else:
-        final_price = "0"
+        final_price = 0
     product_price.append(final_price)
-    print("The", keyword, "is sold at", '${}'.format(product_price[0]))
-# def get_webpage():
-#     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-#     driver.get("https://www.amazon.com/")
-#     # keyword = input("Enter item name: ")
-#     keyword = "PS5"
-#     search_box = driver.find_element(By.ID, 'twotabsearchtextbox')
-#     search_box.send_keys(keyword)
-
-#     search_button = driver.find_element(By.ID, 'nav-search-submit-button')
-#     search_button.click()
-#     driver.implicitly_wait(5)
-#     sleep(5)
-
-#     item_price = driver.find_elements(By.XPATH, './/span[@class="a-price-whole"]')
-#     item_fraction = driver.find_elements(By.XPATH, './/span[@class="a-price-fraction"]')
-
-#     if item_price !=[] and item_fraction !=[]:
-#         final_price = '.'.join([item_price[0].text, item_fraction[0].text])
-#     else:
-#         final_price = 0
-#     product_price.append(final_price)
-#     print( "The", keyword, "is sold at", '${}'.format(product_price[0]))
+    print( "The", keyword, "is sold at", '${}'.format(product_price[0]))
 
 def main():
     global options
